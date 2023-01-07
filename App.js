8d5f35a6400e74ad083fe51a0072839920fe5261
Pro <magus@MacBook-Pro-de-Guy.local>
2023-01-07 17:01:29 +0100

    WIP

--- App.js
+++ App.js
@@ -4,9 +4,9 @@
  import { WebView } from 'react-native-webview';
  import { onMessage } from './utils/messageHandler';
 
- const VOZO_APP_URL = 'https://dev.vozo.app';
+ const VOZO_APP_URL = 'https://www.vozo.app';
  const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
- 
+ const REFRESH_INTERVAL = 1000 * 60 * 5;
 
  const useAppState = () => {
   const [appState, setAppState] = useState(AppState.currentState);
@@ -23,18 +23,27 @@
   return appState;
 }
 
+
  const WebViewWithRef = (props) => {
 
   const webViewRef = useRef(WebView);
   const appState = useAppState();
+  
+
   useEffect(() => {
-    console.log('App state changed', JSON.stringify(appState))
     if (appState === 'active') {
-      console.log('App is active');
-      webViewRef.current.reload();
+      clearInterval(props.refreshInterval.current);
     }
-    else {
-      console.log('App is inactive');
+    else if (appState === 'inactive') {
+      props.refreshInterval.current = setInterval(() => {
+        webViewRef.current.reload();
+      }, REFRESH_INTERVAL);
+    }
+    else if (appState === 'background') {
+      clearTimeout(props.refreshInterval.current);
+      props.refreshInterval.current = setInterval(() => {
+        webViewRef.current.reload();
+      }, REFRESH_INTERVAL);
     }
   }, [appState]);
 
@@ -59,12 +68,13 @@
 
 
  const App = () => {
+  const intervalRef = useRef("");
    return (
      <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
        <View style={{zIndex: 1, backgroundColor: 'black'}}>
          <StatusBar barStyle={'light-content'} backgroundColor="black" />
        </View>
-       <WebViewWithRef />
+       <WebViewWithRef refreshInterval={intervalRef}/>
      </SafeAreaView>
    );
  };
